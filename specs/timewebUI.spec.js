5fe070e6315b95c18ffe119868ae4f8e796aad41
Your Name <you@example.com>
2022-06-20 20:17:13 +0300

    Первый тест

--- specs/timewebUI.spec.js
+++ specs/timewebUI.spec.js
@@ -1,4 +1,7 @@
 import {run, stop} from "../lib/browser";
+import chai from 'chai';
+const assert = chai.assert;
+
 
 describe('UI-тесты для проекта TimeWeb', () => {
     let page
@@ -13,25 +16,29 @@ describe('UI-тесты для проекта TimeWeb', () => {
     const emailField = '.left:nth-child(14) > .columns > .c5-5 > .label > div > .suggestions-input';
     const phoneField = '.c1-2 > .label > .label > div > .phone-mask';
     const registerBtn = '.form > form > .double > .left > .hosting-items__button';
-    const helloMessage = '#p0 > .k-widget > .k-window-content > .welcome-window > .welcome-window__header';
-
+    const exitCross = '.k-widget > .k-window-titlebar > .k-window-actions > .k-window-action > .k-icon';
+    const timewebMainMenu = '#default-layout > .grid__content > .layout-cnt-w > #p0 > .page-start'
+    const createSite = '.layout-cnt-w > #p0 > .page-start > .quick-actions-panel > .quick-actions-panel-item:nth-child(3)';
 
     it ('Регистрация нового пользователя', async () => {
         await page.waitForSelector(tarifYearBtn);
         await page.click(tarifYearBtn);
         await page.waitForSelector(fioField);
         await page.click(fioField);
-        await page.fill(fioField, 'Тест Тест');
+        await page.fill(fioField, 'Шугаев Алексей');
         await page.waitForSelector(emailField);
         await page.click(emailField);
-        await page.fill(emailField, 'amail@mail.ru');
+        await page.fill(emailField, 'saturn98@gmail.com');
         await page.waitForSelector(phoneField);
         await page.click(phoneField);
-        await page.fill(phoneField, '79888888888');
+        await page.fill(phoneField, '79666666666');
         await page.waitForSelector(registerBtn);
         await page.click(registerBtn);
-        await page.waitForSelector(helloMessage);
-        const helloMessageText = await page.textContent(helloMessage);
-        assert(helloMessageText).strictEqual('Поздравляем!', 'Пользователь не зарегистрирован')
+        await page.waitForSelector(exitCross);
+        await page.click(exitCross);
+        await page.waitForSelector(timewebMainMenu);
+        await page.click(timewebMainMenu);
+        const createSiteText = await page.textContent(createSite)
+        assert.equal(createSiteText, 'Создать сайт', 'Пользователь не зарегистрирован')
     })
 })
