734b79a8e8ec6fb7e3548000451c293aea0b8200
Your Name <you@example.com>
2022-06-29 17:53:12 +0300

    Исправления по повторному ревью

--- framework/pages/registrationPage.js
+++ framework/pages/registrationPage.js
@@ -6,10 +6,6 @@ const registerBtn = '.form > form > .double > .left > .hosting-items__button';
 const exitCross = '.k-widget > .k-window-titlebar > .k-window-actions > .k-window-action > .k-icon';
 const timewebMainMenu = '#default-layout > .grid__content > .layout-cnt-w > #p0 > .page-start'
 const createSite = '.layout-cnt-w > #p0 > .page-start > .quick-actions-panel > .quick-actions-panel-item:nth-child(3)';
-const loginField = '.login-form-container > .login > form > .formRow--1LwmF:nth-child(2) > .input--3M8Ak';
-const passwordField = '.login-form-container > .login > form > .formRow--1LwmF:nth-child(3) > .input--3M8Ak';
-const loginBtn = '.login-form-container > .login > form > .formButtonRow--2JbPt > .button--9XBxA';
-const accountName = '.acc-name';
 
 const RegisterPage = {
     registerNewUser: async (page, registerFioValue, registerEmailValue, registerPhoneValue) => {
@@ -31,17 +27,7 @@ const RegisterPage = {
         await page.waitForSelector(timewebMainMenu);
         await page.click(timewebMainMenu);
         const createSiteText = await page.textContent(createSite);
-        console.log(createSiteText)
         return createSiteText;
-    },
-    loginUser: async (page, loginValue, passwordValue) => {
-        await page.click(loginField);
-        await page.fill(loginField, loginValue);
-        await page.click(passwordField);
-        await page.fill(passwordField, passwordValue);
-        await page.click(loginBtn);
-        const accountNameText = await page.textContent(accountName);
-        return accountNameText;
     }
 }
 
